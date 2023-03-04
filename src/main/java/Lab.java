2371cc41a951450d7f2b11b7145682b3c84dcb43
Steven Nwodu <stevennwodu@gmail.com>
2023-03-04 14:06:35 +0000

    week 2 lab complete

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -14,6 +14,11 @@ public class Lab {
      * @return if b is true, return true. if b is false, return false.
      */
     public boolean returnBoolean(boolean b){
+
+        if (b == true) {
+
+            return true;
+        }
         return false;
     }
 
@@ -23,7 +28,21 @@ public class Lab {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnNot(boolean b){
+
+        if (b == true) {
+            
+            return false;
+        }
+        if  (b == false) {
+
+            return true;
+        }
+
         return false;
+        
+
+
+       
     }
 
     /**
@@ -33,6 +52,11 @@ public class Lab {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnAnd(boolean b1, boolean b2){
+
+        if (b1 == true && b2 == true){
+
+            return true;
+        }
         return false;
     }
 
@@ -43,6 +67,11 @@ public class Lab {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnOr(boolean b1, boolean b2){
+
+        if (b1 == true || b2 == true) {
+            
+            return true;
+        }
         return false;
     }
 
